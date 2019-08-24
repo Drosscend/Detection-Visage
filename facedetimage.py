5311bcdd71760b43a185dcf7359db9b910cdd44d
Kévin <47970260+Drosscend@users.noreply.github.com>
2019-08-24 15:54:28 +0200

    furst commit

---
+++ facedetimage.py
@@ -0,0 +1,17 @@
+import cv2
+
+imagePath = "files/groupe1.jpg" # Chemin de l'image
+cascadeClassifierPath = "haarcascade_frontalface_alt.xml" # Chemin du Classifier pour le visage
+
+cascadeClassifier = cv2.CascadeClassifier(cascadeClassifierPath)
+
+image = cv2.imread(imagePath)
+
+grayImage = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY) # Conversion de la vidéo en noir et blanc
+
+detectedFaces = cascadeClassifier.detectMultiScale(grayImage) # Détection des visages
+
+for(x,y,width,height) in detectedFaces:
+	cv2.rectangle(image, (x, y), (x+width, y+height), (0,255,0), 5)
+
+cv2.imwrite('resultat.jpg', image) # crée un fichier "resultat" avec l'image de imagePath.
---
+++ facedetvideo.py
@@ -0,0 +1,28 @@
+import cv2
+import datetime
+
+cascadeClassifierPath = 'haarcascade_frontalface_alt.xml' # Chemin du Classifier pour le visage
+cascadeClassifier = cv2.CascadeClassifier(cascadeClassifierPath)
+
+cap = cv2.VideoCapture(0) # On récupère la webcam
+
+#cap = cv2.VideoCapture("files/video.mp4") # On récupère une vidéo
+
+while(cap.isOpened()):
+	_, frame = cap.read()
+	grayImage = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # Conversion de la vidéo en noir et blanc
+	detectedFaces = cascadeClassifier.detectMultiScale(grayImage,  scaleFactor=1.1, minNeighbors=10, minSize=(20, 20)) # Détection des visages
+
+	for(x,y, width, height) in detectedFaces:
+		cv2.rectangle(frame, (x, y), (x+width, y+height), (0,255,0), 3) # Dessin d'un rectangle autour du visage
+	
+	font = cv2.FONT_HERSHEY_SIMPLEX # police d'écriture pour la date
+	text = str(datetime.datetime.now()) # texte de la date
+	frame = cv2.putText(frame, text, (10,50), font, .7, (0,255,255), 2, cv2.LINE_AA) # Ajout de la date
+
+	cv2.imshow("result", frame)
+	if cv2.waitKey(1) == ord('q'):
+		break
+
+cap.release()
+cv2.destroyAllWindows()
